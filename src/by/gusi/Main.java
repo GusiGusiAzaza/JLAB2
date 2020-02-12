41ac6cacd3e90a22de498332d627b2fd79c93211
Kirill Harevich <kharevich2012@gmail.com>
2020-02-12 14:57:10 +0300

    Initial commit

---
+++ src/by/gusi/Main.java
@@ -0,0 +1,11 @@
+package by.gusi;
+
+import jdk.swing.interop.SwingInterOpUtils;
+//TODO method 0
+public class Main {
+
+    public static void main(String[] args) {
+        //TODO new method 1
+        System.out.println("Hello World 2");
+    }
+}
